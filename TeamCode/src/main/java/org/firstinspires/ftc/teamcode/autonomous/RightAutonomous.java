7275d0af62d8935b6cafc1f63be59bdf274f951d
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2023-02-05 15:40:37 -0600

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RightAutonomous.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Left Autonomous", group = "Autonomous")
+public class LeftAutonomous {
+}
